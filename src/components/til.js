dbc0d68951fe51e329fffed42c299a024b28dcaa
cheolheonShin <cheolheonshin@gmail.com>
2020-07-04 15:28:07 +0900

    [feature] 디렉터리를 2단계로 추가
    기존에는 한단계만 만들었다면 2단계로 만들도록 함

--- src/components/til.js
+++ src/components/til.js
@@ -1,39 +1,63 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-const makeDirs = tils => {
-  let mappedTils = tils.map(til => {
+const makeSubDirs = dirObj => {
+  let keys = Object.keys(dirObj)
+  for (let i = 0; i < keys.length; i++) {
+    let dirArr = dirObj[keys[i]]
+    let obj = {}
+    dirArr.forEach(({ slug, title }) => {
+      let slugArr = slug.split("/")
+      let subDir = slugArr[slugArr.length - 3]
+      if (!obj[subDir]) obj[subDir] = [{ slug, title }]
+      else obj[subDir].push({ slug, title })
+    })
+    dirObj[keys[i]] = obj
+  }
+  return dirObj
+}
+const makeDirs = dirs => {
+  let mappedDirs = dirs.map(til => {
     let slug = til.node.fields.slug
+    let title = til.node.frontmatter.title || ""
     let slugArr = til.node.fields.slug.split("/")
     let majorDir = slugArr[slugArr.length - 4]
-    return { slug, majorDir }
+    return { slug, majorDir, title }
   })
   let obj = {}
-  mappedTils.forEach(({ slug, majorDir }) => {
+  mappedDirs.forEach(({ slug, majorDir, title }) => {
     if (!obj[majorDir]) {
-      obj[majorDir] = [slug]
+      obj[majorDir] = [{ slug, title }]
     } else {
-      obj[majorDir].push(slug)
+      obj[majorDir].push({ slug, title })
     }
   })
+  obj = makeSubDirs(obj)
   return obj
 }
 
-const SubDir = subdir => {
+const EndDir = ({ slug, title }) => {
+  return <Link to={slug}>{title || slug}</Link>
+}
+
+const SubDir = ([subDir, links]) => {
   return (
     <li>
-      <Link to={subdir}>{subdir}</Link>
+      <div>{subDir}</div>
+      <ul>{links.map(EndDir)}</ul>
     </li>
   )
 }
-const MajorDir = ([majorDir, slugs]) => {
+
+const MajorDir = ([majorDir, subDir]) => {
   return (
     <li>
       <div>{majorDir}</div>
-      <ul>{slugs.map(SubDir)}</ul>
+      <ul>{Object.entries(subDir).map(SubDir)}</ul>
     </li>
   )
 }
+
 const Til = ({ dirs }) => {
   return (
     <div>
