6143b0b21b822913c4f8f3b8c96b1c35e17edcbc
cheolheonShin <cheolheonshin@gmail.com>
2020-07-04 14:39:17 +0900

    [feature] 1단계의 디렉터리 자동 생성

--- src/components/til.js
+++ src/components/til.js
@@ -1,12 +1,46 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+
+const SubDir = subdir => {
+  return (
+    <li>
+      <Link to={subdir}>{subdir}</Link>
+    </li>
+  )
+}
+const MajorDir = ([majorDir, slugs]) => {
+  return (
+    <li>
+      <div>{majorDir}</div>
+      <ul>{slugs.map(SubDir)}</ul>
+    </li>
+  )
+}
+
+const makeDirs = tils => {
+  let mappedTils = tils.map(til => {
+    let slug = til.node.fields.slug
+    let slugArr = til.node.fields.slug.split("/")
+    let majorDir = slugArr[slugArr.length - 4]
+    return { slug, majorDir }
+  })
+  let obj = {}
+  mappedTils.forEach(({ slug, majorDir }) => {
+    if (!obj[majorDir]) {
+      obj[majorDir] = [slug]
+    } else {
+      obj[majorDir].push(slug)
+    }
+  })
+  return obj
+}
+
 const Til = ({ tils }) => {
-  console.log(tils)
-  tils.map()
+  let dirs = makeDirs(tils)
   return (
     <div>
-      <hr></hr>
       <h2>TIL</h2>
+      <ul>{Object.entries(dirs).map(MajorDir)}</ul>
     </div>
   )
 }
