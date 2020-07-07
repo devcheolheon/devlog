2a6400f398a6a85898bb50bba9de6fb9a0cb5dec
cheolheonShin <cheolheonshin@gmail.com>
2020-07-07 11:27:38 +0900

    [style] footer에 마진 추가

--- README.md
+++ README.md
@@ -8,6 +8,10 @@
 
 ## 주요 변경 사항    
 
+- v0.1.0
+  - GithubSection 컴포넌트를 추가함 
+    - TIL BIR 레포에서 readme를 긁어온 뒤 디렉터리 구조의 메뉴를 만들어서 웹페이지에서 보여줌
+
 - v0.0.0 
 
   - Gatsby의 공식 스타터인 Gatsby's blog starter([github](https://github.com/gatsbyjs/gatsby-starter-blog)) 에서 출발
--- src/components/layout.js
+++ src/components/layout.js
@@ -58,7 +58,7 @@ const Layout = ({ location, title, children }) => {
     >
       <header>{header}</header>
       <main>{children}</main>
-      <footer>
+      <footer style={{ marginTop: `50px` }}>
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
